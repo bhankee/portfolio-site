619d9e26d45426f2700405bc1f50e72c4cc0e8f7
bradhankee <brad.hankee@gmail.com>
2025-09-12 17:05:17 -0500

    wired up contact email form

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -4,7 +4,20 @@ import { NextResponse } from 'next/server';
 const RESEND_EMAIL_API_KEY = process.env.RESEND_EMAIL_API_KEY;
 
 
-export async function POST() {
+export async function POST(request: Request) {
+
+    const body = await request.json();
+
+    const { message, email } = body;
+
+    // Check if the required fields are present
+    if (!message || !email) {
+        return NextResponse.json(
+            { error: "Missing required fields" },
+            { status: 400 }
+        );
+    }
+
     const res = await fetch('https://api.resend.com/emails', {
         method: 'POST',
         headers: {
@@ -12,15 +25,20 @@ export async function POST() {
             'Authorization': `Bearer ${RESEND_EMAIL_API_KEY}`,
         },
         body: JSON.stringify({
-            from: 'Acme <onboarding@resend.dev>',
-            to: ['delivered@resend.dev'],
-            subject: 'hello world',
-            html: '<strong>it works!</strong>',
+            from: 'Brads Portfolio <onboarding@resend.dev>',
+            to: ['brad.hankee@gmail.com'],
+            subject: 'Email sent from portfolio',
+            html: `<strong>${message}</strong>`,
         }),
     });
 
     if (res.ok) {
         const data = await res.json();
         return Response.json(data);
+    } else {
+        return NextResponse.json(
+            { message: "Email sending failed", error: res.statusText },
+            { status: 400 }
+        );
     }
 }
